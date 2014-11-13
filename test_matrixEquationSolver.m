67419c33f11b52e18d1933441f27b518469daa5f
Alex Townsend <ajt@mit.edu>
2014-11-13 12:37:57 -0500

    Add a test for the matrixEquationSolver code.

---
+++ test_matrixEquationSolver.m
@@ -0,0 +1,45 @@
+function pass = test_matrixEquationSolver( ) 
+% Test matrixEquationSolver.m 
+
+tol = 1e-8; 
+
+n = 10; 
+m = 12; 
+
+A = randn( n ) + 1i*randn(n); 
+B = randn( m ) + 1i*randn(m); 
+EXACT = randn( n, m); 
+F = A * EXACT * B.'; 
+
+X = matrixEquationSolver({A}, {B}, F);
+pass(1) = ( norm( X - EXACT ) < tol ); 
+
+
+C = randn( n ) + 1i*randn(n); 
+D = randn( m ) + 1i*randn(m); 
+EXACT = randn( n, m); 
+F = A * EXACT * B.' + C * EXACT * D.'; 
+X = matrixEquationSolver({A,C}, {B,D}, F);
+pass(2) = ( norm( X - EXACT ) < tol ); 
+
+E = randn( n ) + 1i*randn(n) ; 
+G = randn( m ) + 1i*randn(m); 
+EXACT = randn( n, m); 
+F = A * EXACT * B.' + C * EXACT * D.' + E * EXACT * G.'; 
+X = matrixEquationSolver({A,C,E}, {B,D,G}, F);
+pass(3) = ( norm( X - EXACT ) < tol ); 
+
+
+K = 10;  % number of terms: 
+EXACT = randn( n, m ); 
+F = zeros( n, m ); 
+A = cell(K,1); B = cell(K,1); 
+for j = 1:K 
+    A{j} = randn( n ) + 1i*randn( n ); 
+    B{j} = randn( m ) + 1i*randn( m ); 
+    F = F + A{j} * EXACT * B{j}.'; 
+end
+X = matrixEquationSolver(A, B, F);
+pass(4) = ( norm( X - EXACT ) < tol ); 
+
+end
