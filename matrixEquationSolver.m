0cc220fa18f6bc72c99f232507c056f3344ce2a0
nickhale <nick.p.hale@gmail.com>
2014-11-25 16:53:02 +0200

    Tidy matrixEquationSolver.

--- matrixEquationSolver.m
+++ matrixEquationSolver.m
@@ -1,16 +1,14 @@
 function X = matrixEquationSolver(A, B, F)
-% MATRIXEQUATIONSOLVER 
-%
-% X = matrixEquationSolver(A, B, F) solves the generalized Sylvester
-% equation: 
+%MATRIXEQUATIONSOLVER 
+%   X = MATRIXEQUATIONSOLVER(A, B, F) solves the generalized Sylvester equation:
 %
 %      sum_j A{j} X B{j}^T = F, 
 %
-% where A and B are cell array of nxn and mxm matrices, respectively. 
+%   where A and B are cell array of nxn and mxm matrices, respectively. 
 % 
-% There is no known efficient and general algorithm when the number of terms
-% in the sum is more than 2. The resulting algorithm based on Kronecker 
-% products costs O((mn)^3) operations.  
+%   There is no known efficient and general algorithm when the number of terms
+%   in the sum is more than 2. The resulting algorithm based on Kronecker
+%   products costs O((m*n)^3) operations.
 
 % Written by Alex Townsend, Nov 2014. (alex.townsend1987@gmail.com)
 
@@ -33,23 +31,26 @@ end
 [mB, nB] = size( B{ 1 } ); 
 if ( (mA ~= nA) || (mB ~= nB) )
     error('MATRIXEQUATIONSOLVER:INPUTS:SIZES', ...
-                           'Rectangular matrices are not allowed.'); 
+                       'Rectangular matrices are not allowed.'); 
 end
 
-% If we have two terms, then use bartelsStewart: 
-if  ( ( max(size(A)) == 1 ) )
- % Solving A X B^T = F
+if ( ( max(size(A)) == 1 ) )
+    
+	% Solving A X B^T = F
     Y = A{1} \ F;    % Y = XB^T
     X = Y / B{1}.';  % Solve for X
+    
 elseif ( ( max(size(A)) == 2 ) )
+    % If we have two terms, then use bartelsStewart(): 
     X = bartelsStewart(A{1}, B{1}, A{2}, B{2}, F ); 
+    
 else
     % Form the large Kronecker matrix. 
     
     % Check that the matrices are not too large: 
     if ( nA * nB > maxSize ) 
         error('MATRIXEQUATIONSOLVER:MEMORY', ... 
-                                  'A very large matrix will be formed.' );
+                        'A very large matrix will be formed.' );
     end
 
     % Form the large nA x nB matrix: 
@@ -57,12 +58,11 @@ else
     for j = 1:max(size(A))
         C = C + kron( B{j}, A{j} ); 
     end
-    
     % Solve the large linear system: 
     x = C \ F(:); 
-    
     % Reshape solution vector to a matrix: 
     X = reshape( x, nA, nB ); 
+    
 end
 
 end
--- test_matrixEquationSolver.m
+++ test_matrixEquationSolver.m
@@ -1,10 +1,12 @@
-function pass = test_matrixEquationSolver( ) 
+function pass = test_matrixEquationSolver() 
 % Test matrixEquationSolver.m 
 
 tol = 1e-8; 
 
 n = 10; 
 m = 12; 
+
+%% Test 1
 
 A = randn( n ) + 1i*randn(n); 
 B = randn( m ) + 1i*randn(m); 
@@ -14,6 +16,7 @@ F = A * EXACT * B.';
 X = matrixEquationSolver({A}, {B}, F);
 pass(1) = ( norm( X - EXACT ) < tol ); 
 
+%% Test 2
 
 C = randn( n ) + 1i*randn(n); 
 D = randn( m ) + 1i*randn(m); 
@@ -22,6 +25,8 @@ F = A * EXACT * B.' + C * EXACT * D.';
 X = matrixEquationSolver({A,C}, {B,D}, F);
 pass(2) = ( norm( X - EXACT ) < tol ); 
 
+%% Test 3
+
 E = randn( n ) + 1i*randn(n) ; 
 G = randn( m ) + 1i*randn(m); 
 EXACT = randn( n, m); 
@@ -29,6 +34,7 @@ F = A * EXACT * B.' + C * EXACT * D.' + E * EXACT * G.';
 X = matrixEquationSolver({A,C,E}, {B,D,G}, F);
 pass(3) = ( norm( X - EXACT ) < tol ); 
 
+%% Test 4
 
 K = 10;  % number of terms: 
 EXACT = randn( n, m ); 
